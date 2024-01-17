4a3535c6986aed13478e7e6d5f43ea9f3801e3dd
24saransh <73303168+24saransh@users.noreply.github.com>
2024-01-17 22:56:43 +0530

    Add files via upload

--- Saransh/1.c
+++ Saransh/1.c
@@ -1,1 +1,23 @@
+#include<stdint.h>
+#include<stdio.h>
+#include<stdbool.h>
+typedef struct S{
+    uint32_t a;
+    short b;
+    char *str;
+    bool flag;
+};
 
+int main()
+{
+    struct S obj1;
+    struct S* ptr = &obj1;
+    ptr->a=69;
+    ptr->b=145;
+    ptr->str = "Hello NXP";
+    ptr->flag = true;
+    printf("%d\n",ptr->a);
+    printf("%d\n",ptr->b);
+    printf("%s\n",ptr->str);
+    printf("%d\n",ptr->flag);
+}
